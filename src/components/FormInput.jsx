9fc94d1bf904cc8a6e1ff30bf5b4c05e0c616134
Denis Tovstokhatko <tovstokhatko.denis@gmail.com>
2017-07-21 15:24:46 +0300

    add more components

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,14 +4,43 @@ import Suggest from './Suggest';
 
 class FormInput extends Component {
   getValue() {}
-  render() {}
+  render() {
+    const common = {
+      id: this.props.id,
+      ref: 'input',
+      defaultValue: this.props.defaultValue,
+    };
+
+    switch (this.props.type) {
+      case 'year':
+        return (
+          <input
+          {...common}
+          type="number"
+          defaultValue={this.props.defaultValue ||
+          new Date().getFullYear()} />
+        );
+      case 'suggest':
+        return <Suggest {...common} options={this.props.options} />;
+      case 'rating':
+        return (
+          <Rating
+          {...common}
+          defaultValue={parseInt(this.props.defaultValue,
+          10)} />
+        );
+      case 'text':
+        return <textarea {...common} />;
+      default:
+        return <input {...common} type="text" />;
+    }
+  }
 }
 
 FormInput.propTypes = {
   type: PropTypes.oneOf(['year', 'suggest', 'rating', 'text', 'input']),
   id: PropTypes.string,
   options: PropTypes.array,
-  // как в вариантах автозаполнения <option>
   defaultValue: PropTypes.any
 };
 
